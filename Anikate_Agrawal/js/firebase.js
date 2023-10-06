6027116c0945375d3ee6d2ff310a96782ad64cc8
anikateagrawal <anikateag7316@gmail.com>
2023-10-06 22:56:32 +0530

    commit

---
+++ Anikate_Agrawal/js/firebase.js
@@ -0,0 +1,13 @@
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+  
+  const firebaseConfig = {
+    apiKey: "AIzaSyBhHMpH6D40hy0eZZ17Qgy51zfVdL2526U",
+    authDomain: "web-app-bc3f6.firebaseapp.com",
+    projectId: "web-app-bc3f6",
+    storageBucket: "web-app-bc3f6.appspot.com",
+    messagingSenderId: "809223472069",
+    appId: "1:809223472069:web:2c067c5b0b1882fe8a01ac"
+  };
+
+  const app = initializeApp(firebaseConfig);
